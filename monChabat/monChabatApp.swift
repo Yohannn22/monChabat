d56297c831836e16ce873ff5822debe8c4b33afc
Yohann NEZRI <yohann@me.com>
2025-12-25 15:37:58 +0100

    Initial Commit

---
+++ monChabat/monChabatApp.swift
@@ -0,0 +1,17 @@
+//
+//  monChabatApp.swift
+//  monChabat
+//
+//  Created by Yohann Nezri on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct monChabatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ monChabatTests/monChabatTests.swift
@@ -0,0 +1,17 @@
+//
+//  monChabatTests.swift
+//  monChabatTests
+//
+//  Created by Yohann Nezri on 25/12/2025.
+//
+
+import Testing
+@testable import monChabat
+
+struct monChabatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
